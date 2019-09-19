d70c4b51e75f117d8f7f896c1bd764f33ac7bbd6
Brooke McMillan <Brooke.McMillan@improving.com>
2019-09-19 12:05:48 -0500

    Add Jump and Inventory Command

--- src/main/java/org/improving/tag/Game.java
+++ src/main/java/org/improving/tag/Game.java
@@ -1,5 +1,8 @@
 package org.improving.tag;
 
+import org.improving.tag.commands.DanceCommand;
+import org.improving.tag.commands.InventoryCommand;
+import org.improving.tag.commands.JumpCommand;
 import org.improving.tag.commands.LookCommand;
 
 import java.util.Date;
@@ -9,12 +12,18 @@ public class Game {
     private Date startTime;
     private Date endTime;
 
-    public Date getStartTime() { return startTime; }
+    public Date getStartTime() {
+        return startTime;
+    }
+
     private void setStartTime(Date startTime) {
         this.startTime = startTime;
     }
 
-    public Date getEndTime() { return endTime; }
+    public Date getEndTime() {
+        return endTime;
+    }
+
     private void setEndTime(Date endTime) {
         this.endTime = endTime;
     }
@@ -24,29 +33,32 @@ public class Game {
         this.setStartTime(new Date());
 
 
-
         boolean loop = true;
         while (loop) {
             System.out.print("> ");
             String input = scanner.nextLine();
             input = input.trim();
             LookCommand lCmd = new LookCommand();
+            DanceCommand dCmd = new DanceCommand();
+            InventoryCommand iCmd = new InventoryCommand();
+            JumpCommand jCmd = new JumpCommand();
             if (lCmd.isValid(input)) {
-                input.trim();
                 lCmd.execute(input);
-            } else if (input.equals("inventory")) {
-                System.out.println("You are carrying nothing.");
-            } else if (input.equals("dance")) {
-                System.out.println("You dance around.");
-            } else if (input.equals("jump")) {
-                System.out.println("You jump in the air.");
+            } else if (iCmd.isValid(input)) {
+                iCmd.execute(input);
+            } else if (dCmd.isValid(input)) {
+                dCmd.execute(input);
+            } else if (jCmd.isValid(input)) {
+                jCmd.execute(input);
             } else if (input.equals("exit")) {
                 System.out.println("Goodbye.");
                 loop = false;
-            } else {
+            } else
                 System.out.println("Huh, I don't understand?");
-            }
         }
         this.setEndTime(new Date());
+
     }
 }
+
+
