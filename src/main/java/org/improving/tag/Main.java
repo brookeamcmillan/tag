8b5474b63080512e3685f081d3a51433a340eaaf
Brooke McMillan <Brooke.McMillan@improving.com>
2019-09-20 09:58:12 -0500

    Implemented SpringContext

--- src/main/java/org/improving/tag/Main.java
+++ src/main/java/org/improving/tag/Main.java
@@ -1,14 +1,16 @@
 package org.improving.tag;
 
-import java.util.Scanner;
+import org.improving.tag.commands.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args)  {
-        System.out.println("Start of Main");
-        Game game = new Game();
-        System.out.println("Declared game");
-       game.run();
-       System.out.println("After Run()");
+
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(SpringContext.class);
+
+        Game game= context.getBean(Game.class);
+        game.run();
 
         long elapsedTicks = game.getStartTime().getTime() - game.getEndTime().getTime();
         double elapsedSeconds = elapsedTicks/1000.00;
--- src/main/java/org/improving/tag/commands/DanceCommand.java
+++ src/main/java/org/improving/tag/commands/DanceCommand.java
@@ -1,7 +1,11 @@
 package org.improving.tag.commands;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class DanceCommand extends BaseEmoteCommand{
     public DanceCommand() {
         super("dance", "You dance around.");
     }
 }
+
